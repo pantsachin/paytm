235b70a4aa33030ff4e635a9d571488fcc66c531
Sachin Pant <74765996+pantsachin@users.noreply.github.com>
2024-04-05 16:19:41 +0530

    Final Solution Coded
    I will try testing the API locally now

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,6 +1,67 @@
 const express = require('express'); 
-
 const router = express.Router();
+const Account = require("../db");
+const { authMiddleware } = require('./middleware');
+ 
+
+router.get("/balance", authMiddleware, async (req, res) => {
+    const account = await Account.findOne({
+        userId: req.userId
+    })
+
+    res.json({
+        balance: account.balance
+    })
+})
+
+
+router.post("/transfer", authMiddleware, async (req, res) => {
+    const {amount, to} = req.body; 
+
+    const account = await Account.findOne({
+        userId: req.userId
+    })
+
+    if (account.balance < amount) {
+        return res.status(400).json({
+            message: "Insufficient Balance"
+        })
+    }
+
+    const toAccount = await Account.findOne({
+        userId: to
+    })
+
+    if (!toAccount) {
+        return res.status(400).json({
+            message: "Invalid Account"
+        })
+    }
+
+    await Account.updateOne({
+        userId: req.userId
+    }, {
+        $inc: {
+            balance: -amount
+        }
+    })
+
+    await Account.updateOne({
+        userId: to
+    }, 
+    {
+        $inc: {
+            balance: amount
+        }
+    })
+
+    res.json({
+        message: "Transfer Successfull"
+    })
+
+
+} )
+
 
 
 module.exports = router;
