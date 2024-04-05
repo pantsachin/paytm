9064f4c26aa6de924fc1c58fcb96afbd67cafa9f
Sachin Pant <74765996+pantsachin@users.noreply.github.com>
2024-04-05 13:15:52 +0530

    practice commit
    Committing after a lot of practice

--- backend/routes/middleware.js
+++ backend/routes/middleware.js
@@ -5,33 +5,30 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    if (!autHeader || !authHeader.startsWith('Bearer')) {
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
         return res.status(403).json({
             message: "Doesn't have the bearer token"
-        })
+        });
     }
 
     const token = authHeader.split(' ')[1];
 
     try {
-        const decoded = jwt.verify(token, JWT_SECRET);
+        const decoded = jwt.verify(tojen, JWT_SECRET);
 
-        if (decoded.userId) {
-            req.userId = decoded.userId;
-            next();
-        } else {
-            return res.status(403).json({
-                message: "Soemthing is not right"
-            })
-        }
-        
+        req.userId = decoded.userId;
+
+        next();
     } catch (err) {
-        return res.status(403).json({})
+        return res.status(403).json({
+            message: "Something is not right"
+        })
     }
+
 
 }
 
-modile.exports({
+module.exports = {
     authMiddleware
-})
+}
 
