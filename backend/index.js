6facdcce3e30dbbb7d5ee8d63a7967682fa6245b
Sachin Pant <74765996+pantsachin@users.noreply.github.com>
2024-04-09 13:04:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,4 +15,5 @@ const jwt = require('jsonwebtoken');
 
 app.use('/api/v1', mainRouter)
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
