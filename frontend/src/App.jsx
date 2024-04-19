223d04f77466759ea25fae5bed71b355b72b9701
Sachin Pant <74765996+pantsachin@users.noreply.github.com>
2024-04-19 11:52:19 +0530

    Commit with Balance and AppBar Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,25 +2,29 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { BottomWarning } from './components/BottomWarning';
 import { Signup } from "./pages/Signup"
 import { Signin } from './pages/Signin';
+import { AppBar } from './components/Appbar';
+import { Balance } from './components/Balance';
 
 
 function App() {
 
   return (
     <div>
-        <BrowserRouter>
+        {/* <BrowserRouter>
 
         <Routes>
 
         <Route path="/signup" element={<Signup/>} />
         <Route path="/signin" element={<Signin/>}  />
-        {/* <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/send" element={<SendMoney/>} /> */}
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/send" element={<SendMoney/>} />
         
         </Routes>
 
-        </BrowserRouter>
-  
+        </BrowserRouter> */}
+
+        <Balance/>
+
 
     </div>
   )
